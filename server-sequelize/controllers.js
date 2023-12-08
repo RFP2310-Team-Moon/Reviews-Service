e3c796a29211e2caf6ae154b0a1cda843eaa4f40
Christian Lee <christianlee4037@gmail.com>
2023-12-07 19:56:52 -0800

    Setup postgres connection

---
+++ server-sequelize/controllers.js
@@ -0,0 +1,11 @@
+const db = require("./db");
+
+module.exports = {
+  reviews: {
+    getReviews: (req, res) => {},
+    postReview: (req, res) => {},
+    report: (req, res) => {},
+    markHelpful: (req, res) => {},
+    getMeta: (req, res) => {},
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,17 +1,32 @@
 const express = require("express");
-const path = require("path");
-const router = require("./router");
+const bodyParser = require("body-parser");
+const db = require("./db");
 
 const app = express();
-const port = process.env.PORT;
 
-app.use(express.json());
-app.use("/", router);
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.listen(port, (err) => {
+app.get("/", async (req, res) => {
+  try {
+    const qString = "SELECT * FROM REVIEWS LIMIT 10;";
+    const result = await db.query(qString);
+    console.log(result);
+    res.json(result.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(process.env.PORT, (err) => {
   if (err) {
     console.log("Error starting server");
   } else {
-    console.log("Server running on port: ", port);
+    console.log("Server running on port: ", process.env.PORT);
   }
 });
